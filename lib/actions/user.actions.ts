80a627e796ffcfb74f56890e4733f296dc043d6f
Sudhiksha03v <ksha33yesvee@gmail.com>
2024-12-28 12:32:06 +0530

    first commit

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,73 @@
+'use server';
+
+import { clerkClient } from "@clerk/nextjs/server";
+import { parseStringify } from "../utils";
+import { liveblocks } from "../liveblocks";
+
+interface ClerkUser {
+  id: string;
+  firstName: string;
+  lastName: string;
+  emailAddresses: { emailAddress: string }[];
+  imageUrl: string;
+}
+
+interface GetUserListResponse {
+  data: ClerkUser[]; // Assuming 'data' is an array of ClerkUser objects
+}
+
+export const getClerkUsers = async ({ userIds }: { userIds: string[] }) => {
+  try {
+    const client = await clerkClient();
+    const response = (await client.users.getUserList({
+      emailAddress: userIds,
+    })) as GetUserListResponse; // Explicitly typing the response
+
+    const users = response.data.map((user: ClerkUser) => ({
+      id: user.id,
+      name: `${user.firstName} ${user.lastName}`,
+      email: user.emailAddresses[0]?.emailAddress || "", // Ensure safety with optional chaining
+      avatar: user.imageUrl,
+    }));
+
+    const sortedUsers = userIds.map((email) =>
+      users.find((user) => user.email === email)
+    );
+
+    return parseStringify(sortedUsers);
+  } catch (error) {
+    console.log(`Error fetching users: ${error}`);
+  }
+};
+
+export const getDocumentUsers = async ({
+  roomId,
+  currentUser,
+  text,
+}: {
+  roomId: string;
+  currentUser: string;
+  text: string;
+}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+
+    const users = Object.keys(room.usersAccesses).filter(
+      (email) => email !== currentUser
+    );
+
+    if (text.length) {
+      const lowerCaseText = text.toLowerCase();
+
+      const filteredUsers = users.filter((email: string) =>
+        email.toLowerCase().includes(lowerCaseText)
+      );
+
+      return parseStringify(filteredUsers);
+    }
+
+    return parseStringify(users);
+  } catch (error) {
+    console.log(`Error fetching document users: ${error}`);
+  }
+};
